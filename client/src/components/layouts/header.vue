0769d0352b4bd8bbc1613dfa060fde8e6e232301
LiJun <lij@terminus.io>
2018-05-20 12:48:54 +0800

    add default layout

---
+++ client/src/components/layouts/header.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="my-header">header</div>
+</template>
+
---
+++ client/src/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="app">
+    <my-header>
+
+    </my-header>
+
+      <nuxt/>
+
+    <my-footer>
+
+
+    </my-footer>
+  </div>
+</template>
+
+<script>
+import myHeader from '~/components/layouts/header';
+import myFooter from '~/components/layouts/footer';
+
+export default {
+
+  components: {
+    myHeader,
+    myFooter,
+  }
+
+}
+</script>
