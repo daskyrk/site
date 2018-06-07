be6676541fde8ea3dbdd74df7627ad1c1a920dc9
LiJun <lij@terminus.io>
2018-06-07 21:44:36 +0800

    update form render

---
+++ client/src/components/common/form-render.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-form-renderer :content="fields" ref='form'><slot /></el-form-renderer>
+</template>
+
+<script>
+import ElFormRenderer from 'el-form-renderer';
+
+export default {
+  components: {
+    ElFormRenderer,
+  },
+
+  props: {
+    fields: Array,
+  },
+
+  methods: {
+    callFormFn(fn, ...args) {
+      return this.$refs['form'][fn](...args);
+    },
+  },
+};
+</script>
+
