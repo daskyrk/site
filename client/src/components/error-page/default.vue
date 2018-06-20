90f69ee0196c5293cc187ecea06a4b8cf06fa149
LiJun <lij@terminus.io>
2018-06-20 20:23:01 +0800

    add different error page

---
+++ client/src/components/error-page/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="error-page">
+    <h1>An error occured {{error}}</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+
+<style scoped>
+.error-page {
+  padding-top: 10%;
+  text-align: center;
+}
+</style>
--- client/src/layouts/error.vue
+++ client/src/layouts/error.vue
@@ -1,24 +1,22 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occured{{error}}</h1>
-    <nuxt-link to="/">Home page</nuxt-link>
-  </div>
+  <component v-bind:is="comp"></component>
 </template>
 
 <script>
-export default {
-  props: ['error']
-}
-</script>
+import Default from '~/components/error-page/default';
+import Page404 from '~/components/error-page/404';
+import Page502 from '~/components/error-page/502';
 
-<style scoped>
-.container {
-  font-family: sans-serif;
-  padding-top: 10%;
-  text-align: center;
-}
-h1 {
-  font-size: 20px;
-}
-</style>
+export default {
+  props: ['error'],
+
+  computed: {
+    comp() {
+      return {
+        404: Page404,
+        502: Page502,
+      }[this.error.statusCode] || Default;
+    },
+  },
+};
+</script>
