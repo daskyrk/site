697766098f93c6c4b5e979875723ef292f32a25b
LiJun <lij@terminus.io>
2018-07-09 23:19:19 +0800

    local use my-config

--- server/lib/config.js
+++ server/lib/config.js
@@ -1,24 +1,28 @@
-exports.MONGODB = {
-  type: 'mongodb',
-  uris: 'mongodb://127.0.0.1:27017',
-  options: {},
-};
+const myConfig = require('./my-config');
+module.exports = myConfig
 
-exports.APP = {
-  port: 8000,
-  prefix: '/api',
-  LIMIT: 10,
-};
+// 以下为my-config文件内容示例
+// exports.MONGODB = {
+//   type: 'mongodb',
+//   uris: 'mongodb://127.0.0.1:27017',
+//   options: {},
+// };
 
-exports.AUTH = {
-  secretKey: 'your secretKey',
-};
+// exports.APP = {
+//   port: 8000,
+//   prefix: '/api',
+//   LIMIT: 10,
+// };
 
-exports.UPLOAD = {
-  accessKey: 'your accessKey',
-  secretKey: 'your secretKey',
-  bucket: 'bucket name',
-  origin: 'http://blog.u.qiniudn.com',
-  uploadURL: 'http://up.qiniu.com/',
-  // timeout: 3600000, // default rpc timeout: one hour, optional
-};
+// exports.AUTH = {
+//   secretKey: 'your secretKey',
+// };
+
+// exports.UPLOAD = {
+//   accessKey: 'your accessKey',
+//   secretKey: 'your secretKey',
+//   bucket: 'bucket name',
+//   origin: 'http://blog.u.qiniudn.com',
+//   uploadURL: 'http://up.qiniu.com/',
+//   // timeout: 3600000, // default rpc timeout: one hour, optional
+// };
