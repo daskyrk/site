7253f3f6593cf006ace93b6ae8e1bb65c60be1b0
LiJun <lj167963@alibaba-inc.com>
2019-04-07 11:25:05 +0800

    feat: transform _id to id when output

--- server/src/shared/base/base.schema.ts
+++ server/src/shared/base/base.schema.ts
@@ -4,7 +4,11 @@ export const schemaOptions: SchemaOptions = {
   toJSON: {
     virtuals: true,
     getters: true,
-    versionKey: false,
+    transform (doc, ret, options) {
+      ret.id = ret._id;
+      delete ret._id;
+      delete ret.__v;
+    },
   },
   timestamps: true,
 };
