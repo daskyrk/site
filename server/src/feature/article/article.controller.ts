65b4f893af42a9765c96b4951c7591bed87d2775
LiJun <lj167963@alibaba-inc.com>
2019-04-07 14:08:27 +0800

    feat: apply auth guard to article post

--- server/src/feature/article/article.controller.ts
+++ server/src/feature/article/article.controller.ts
@@ -1,14 +1,17 @@
 import { ArticleInfoDto, QueryArticleDto } from './dto/article.dto';
+import { AuthGuard, RolesGuard } from '@/core/guards';
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import { Roles, RolesEnum } from '@/core/decorators';
 
 import { ArticleService } from './article.service';
-import { AuthGuard } from '@/core/guards/auth.guard';
 
 @Controller('article')
 export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Post()
+  // @Roles(RolesEnum.Admin)
+  @UseGuards(AuthGuard)
   public create(@Body() articleInfoDto: ArticleInfoDto) {
     return this.articleService.create(articleInfoDto);
   }
