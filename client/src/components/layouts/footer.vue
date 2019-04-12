4859b412ee6ebff9a33bee4b84b1a7074d1dc3cc
LiJun <lj167963@alibaba-inc.com>
2019-04-12 22:32:37 +0800

    feat: update footer style

--- client/src/components/layouts/footer.vue
+++ client/src/components/layouts/footer.vue
@@ -1,22 +1,43 @@
 <template>
   <footer class="default-footer">
-    <div class="footer-link">
-      <template v-for="(item, i) in footer.data">
-        {{ i === 0 ? '' : footer.split }}
-        <nuxt-link
-          v-if="item.link"
-          :key="i"
-          :to="item.link"
-        >
-          {{ item.text }}
-        </nuxt-link>
-        <span
-          v-else
-          :key="i"
-        >
-          {{ item.text }}
-        </span>
-      </template>
+    <div class="footer-container">
+      <div class="footer-col copy-right">
+        <p>
+          <nuxt-link to="/about">
+            @daskyrk
+          </nuxt-link>
+        </p>
+        <p>Powered by Nuxt</p>
+        <p>已运行 10个月 3天 12小时 43分</p>
+
+        <!-- <div class="footer-link">
+          <template v-for="(item, i) in footer.data">
+            {{ i === 0 ? '' : footer.split }}
+            <nuxt-link
+              v-if="item.link"
+              :key="i"
+              :to="item.link"
+            >
+              {{ item.text }}
+            </nuxt-link>
+            <span
+              v-else
+              :key="i"
+            >
+              {{ item.text }}
+            </span>
+          </template>
+        </div> -->
+      </div>
+
+      <div class="footer-col friend-link">
+        <p>友链：</p>
+        <p>
+          <nuxt-link to="/about">
+            我的小窝
+          </nuxt-link>
+        </p>
+      </div>
     </div>
   </footer>
 </template>
@@ -34,14 +55,35 @@ export default {
 
 <style lang="scss">
 .default-footer {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: $footer-height;
+  padding: 2rem 0;
   background: $white;
-  border-top: 1px solid $color-border;
 
-  .footer-link {
+  .footer-container {
+    display: flex;
+    width: 100%;
+    max-width: 960px;
+    height: 100%;
+    margin: 0 auto;
+    padding: 0 5rem;
+    color: $color-text-sub;
+    font-size: 13px;
+  }
+
+  p {
+    margin-bottom: $p4;
+  }
+
+  .footer-col {
+    flex: 1;
+    padding: 0 2rem;
+  }
+
+  .copy-right {
+    margin: 0;
+    line-height: 20px;
+  }
+
+  .friend-link {
     > a,
     > span {
       padding: 0 1rem;
@@ -49,7 +91,7 @@ export default {
   }
 
   a:hover {
-    color: $color-primary;
+    color: $color-active-red;
   }
 }
 </style>
