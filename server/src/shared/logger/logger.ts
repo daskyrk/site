dba5bb6b49491b47e306eb1b1b8af04705edd81d
LiJun <lj167963@alibaba-inc.com>
2019-05-04 13:26:05 +0800

    refactor: remove useless config encode

--- server/src/shared/logger/logger.ts
+++ server/src/shared/logger/logger.ts
@@ -1,7 +1,28 @@
 import { Injectable, LoggerService } from '@nestjs/common';
 import log4js, { Logger } from 'log4js';
 
-import { LOG4J } from '@/config/config';
+import path from 'path';
+
+const LOG4J = {
+  appenders: {
+    out: { type: 'console' },
+    app: {
+      type: 'dateFile',
+      filename: path.join('./logs', 'BLOG_LOGGER'),
+      pattern: '-yyyy-MM-dd.log',
+      alwaysIncludePattern: true,
+      appender: {
+        type: 'console',
+      },
+    },
+  },
+  categories: {
+    default: {
+      appenders: ['out', 'app'],
+      level: 'debug',
+    },
+  },
+};
 
 @Injectable()
 export class BlogLogger implements LoggerService {
