4ee64fc273533aab6608ccc5baca7845286a7f40
LiJun <lj167963@alibaba-inc.com>
2019-06-01 19:20:40 +0800

    feat: add table op btn component

---
+++ admin/src/components/global/table-op-btn.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-tooltip
+    v-if="tip"
+    top
+    content-class="top"
+  >
+    <v-btn
+      slot="activator"
+      class="v-btn--simple"
+      :color="color || 'success'"
+      icon
+      v-bind="btnProps"
+      v-on="$listeners"
+    >
+      <slot />
+    </v-btn>
+    <span>{{ tip }}</span>
+  </v-tooltip>
+  <v-btn
+    v-else
+    class="v-btn--simple"
+    :color="color || 'success'"
+    icon
+    v-bind="btnProps"
+    v-on="$listeners"
+  >
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  data() {
+    const { tip, color, ...btnProps } = this.$attrs
+    return {
+      tip,
+      color,
+      btnProps,
+    }
+  },
+}
+</script>
