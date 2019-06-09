557e3104a73dc76c9e9a92f8a552f575cfcbb236
LiJun <lj167963@alibaba-inc.com>
2019-06-09 16:03:21 +0800

    fix: copy plugin

--- client/src/plugins/copy.client.js
+++ client/src/plugins/copy.client.js
@@ -1,12 +1,14 @@
+import { appConfig } from '@@/config';
+
 if (process.env.NODE_ENV === 'production') {
   function getCopyright(split = '\n', wrapBefore = '\n', wrapAfter = '') {
     return [
       wrapBefore,
       '著作权归作者所有。',
       '商业转载请联系作者获得授权，非商业转载请注明出处。',
-      '作者：' + process.env.owner || 'lijun',
+      '作者：' + appConfig.owner || 'lijun',
       '链接：' + location.href,
-      '来源：' + process.env.site || 'https://lijun.space',
+      '来源：' + appConfig.site || 'https://lijun.space',
       wrapAfter,
     ].join(split)
   }
